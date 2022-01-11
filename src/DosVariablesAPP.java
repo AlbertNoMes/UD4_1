5c682ee69f150f780efba20d6764eb3cc632e28b
AlbertNoMes <albertnotariomestres@gmail.com>
2022-01-11 23:16:28 +0100

    Dos Variables

---
+++ src/DosVariablesAPP.java
@@ -0,0 +1,21 @@
+
+public class DosVariablesAPP {
+
+	public static void main(String[] args) {
+		
+		int a=10, b=2;
+		int suma=a+b;
+		int resta=a-b;
+		int multi=a*b;
+		int divi=a/b;
+		int modul=a%b;
+		
+		System.out.println("La suma es "+suma);
+		System.out.println("La resta es "+resta);
+		System.out.println("La multiplicación es "+multi);
+		System.out.println("La división es "+divi);
+		System.out.println("El resto de la división es "+modul);
+
+	}
+
+}
