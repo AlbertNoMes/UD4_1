5a6f9cec0bfae2dd80d3606eeb52d5ed6ab215a4
AlbertNoMes <albertnotariomestres@gmail.com>
2022-01-16 17:06:56 +0100

    DosVariables reducido

--- src/DosVariablesAPP.java
+++ src/DosVariablesAPP.java
@@ -4,18 +4,11 @@ public class DosVariablesAPP {
 	public static void main(String[] args) {
 		
 		int a=10, b=2;
-		int suma=a+b;
-		int resta=a-b;
-		int multi=a*b;
-		int divi=a/b;
-		int modul=a%b;
 		
-		System.out.println("La suma es "+suma);
-		System.out.println("La resta es "+resta);
-		System.out.println("La multiplicación es "+multi);
-		System.out.println("La división es "+divi);
-		System.out.println("El resto de la división es "+modul);
-
+		System.out.println("La suma es "+(a+b));
+		System.out.println("La resta es "+(a-b));
+		System.out.println("La multiplicación es "+(a*b));
+		System.out.println("La división es "+(a/b));
+		System.out.println("El resto de la división es "+(a%b));
 	}
-
 }
